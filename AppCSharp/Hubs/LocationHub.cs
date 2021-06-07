105dd39cab9e545b3a61ba40748c2f5b311f1c16
quannguyent <49954085+quannguyent@users.noreply.github.com>
2021-06-07 12:53:22 +0700

    add hub

--- AppCSharp/Hubs/LocationHub.cs
+++ AppCSharp/Hubs/LocationHub.cs
@@ -1,20 +1,32 @@
 ﻿using LocatingApp.Entities;
+using LocatingApp.Services.MAppUser;
 using LocatingApp.Services.MLocationLog;
 using Microsoft.AspNetCore.SignalR;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace LocatingApp.Hubs
-{
+{ 
     public class LocationHub : Hub
     {
         private ILocationLogService LocationLogService;
-        public LocationHub(ILocationLogService LocationLogService)
+        private IAppUserService AppUserService;
+        public LocationHub(ILocationLogService LocationLogService,
+            IAppUserService AppUserService)
         {
+            this.AppUserService = AppUserService;
             this.LocationLogService = LocationLogService;
         }
         public async Task SendLocation(long UserId)
         {
-            var LocationLog = new LocationLog();
+            var Friends = await AppUserService.ListFriends(UserId);
+            var LocationLog = Friends.Select(x => new
+            {
+                x.Id,
+                x.Avatar,
+                Longtitude = x.LocationLogs.Select(x => x.Longtitude).FirstOrDefault(),
+                Latitude = x.LocationLogs.Select(x => x.Latitude).FirstOrDefault(),
+            });
             await Clients.Caller.SendAsync("ReceiveFriendLocation", LocationLog);
         }
     }
